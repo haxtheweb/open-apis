b2397f75f3e7bda0d131315c750bda4519314155
btopro <bto108@psu.edu>
2025-02-18 15:44:02 -0500

    https://github.com/haxtheweb/issues/issues/2230

---
+++ api/apps/haxcms/convert/recipeToSite.js
@@ -0,0 +1,49 @@
+// duckduckgo.js
+// this is an example to fork from that uses common, simple conventions
+// for getting data, validating data, and responding in a consistent way.
+import { stdPostBody, stdResponse, invalidRequest } from "../../../utilities/requestHelpers.js";
+import * as child_process from "child_process";
+import * as util from "node:util";
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+const exec = util.promisify(child_process.exec);
+const SITENAME = 'mysite';
+const RECIPENAME = 'tmp.recipe';
+const ITEMSFILE = 'items.json';
+
+export default async function handler(req, res) {
+  // destructing GET params after ? available in this object
+  // use this if POST data is what's being sent
+  let body = {};
+  let q = null;
+  if (req && req.query && req.query.q) {
+    body = req.query;
+  }
+  else {
+    body = stdPostBody(req);
+  }
+  // fallback support for post
+  if (body && body.q) {
+    q = body.q;
+  }
+  // need to know what we're searching for otherwise bail
+  if (q) {
+    await exec(`hax site ${SITENAME} --y --quiet`);
+    // we import fetch just to simplify endpoint creation but its just fetch
+    const recipe = await fetch(`${q}`).then((d) => d.ok ? d.text(): {});
+    fs.writeFileSync(`${process.cwd()}/${SITENAME}/${RECIPENAME}`, recipe);
+    await exec(`hax site recipe:play --y --recipe "${RECIPENAME}" --root "${process.cwd()}/${SITENAME}"`);
+
+    await exec(`hax site site:items --y --format json --to-file "${ITEMSFILE}" --root "${process.cwd()}/${SITENAME}"`);
+    const items = JSON.parse(fs.readFileSync(`${process.cwd()}/${SITENAME}/${ITEMSFILE}`, 'utf8'));
+    res = stdResponse(res, items, {cache: 86400, methods: "OPTIONS, POST, GET" });
+  }
+  else {
+    // invalidate the response and provide a reason
+    // this optionally takes in a status code otherwise default is 400
+    // vercel will through a 500 if there was any bricking issue so we don't
+    // need to throw that most likely
+    res = invalidRequest(res, 'missing `q` param');
+  }
+}
