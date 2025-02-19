0b606f3fe620dd2592d8e51402938074fabac24f
btopro <bto108@psu.edu>
2025-02-19 16:52:22 -0500

    https://github.com/haxtheweb/issues/issues/2230

--- api/apps/haxcms/convert/recipeToSite.js
+++ api/apps/haxcms/convert/recipeToSite.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
   }
   // need to know what we're searching for otherwise bail
   if (q) {
-    const HAXPROGRAM = 'cd /tmp/ && npx @haxtheweb/create --';
+    const HAXPROGRAM = 'hax';
     await exec(`${HAXPROGRAM} site ${SITENAME} --path "/tmp/" --y --quiet`);
     // we import fetch just to simplify endpoint creation but its just fetch
     const recipe = await fetch(`${q}`).then((d) => d.ok ? d.text(): {});
