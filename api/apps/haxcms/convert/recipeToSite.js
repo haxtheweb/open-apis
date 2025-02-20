9bf0989bf32b3a8d3b7f98098e0366aac1efbe40
btopro <bto108@psu.edu>
2025-02-20 16:30:14 -0500

    more file loc tests

--- api/apps/haxcms/convert/recipeToSite.js
+++ api/apps/haxcms/convert/recipeToSite.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
   // need to know what we're searching for otherwise bail
   if (q) {
     const { stdout, stdin } = await exec("npm root");
-    const HAXPROGRAM = `${stdout.trim()}/.bin/hax`;
+    const HAXPROGRAM = `${stdout.trim()}/@haxtheweb/create/dist/create.js`;
     await exec(`${HAXPROGRAM} site ${SITENAME} --path "/tmp/" --y --quiet`);
     // we import fetch just to simplify endpoint creation but its just fetch
     const recipe = await fetch(`${q}`).then((d) => d.ok ? d.text(): {});
