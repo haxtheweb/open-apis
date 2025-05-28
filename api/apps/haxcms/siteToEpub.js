3045923e7fdc4b6c842b93ee880ddf2c9d709925
btopro <bto108@psu.edu>
2025-05-28 15:59:40 -0400

    https://github.com/haxtheweb/issues/issues/2299 backend piece for testing

--- api/apps/haxcms/siteToEpub.js
+++ api/apps/haxcms/siteToEpub.js
@@ -1,9 +1,12 @@
 import { stdPostBody, stdResponse } from "../../utilities/requestHelpers.js";
 import { JSONOutlineSchema } from "./lib/JSONOutlineSchema.js";
-
+import { encode } from "base64-arraybuffer";
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import url from "url";
-import Epub from "epub-gen";
+import * as EPUB from "epub-gen-memory";
 
+const epub = EPUB.default.default;
 // site object to validate response from passed in url
 export default async function handler(req, res) {
   let content = '';
@@ -23,21 +26,40 @@ export default async function handler(req, res) {
     await site.load(`${base}/site.json`);
     // load all pages for their content
     const items = await pagesAsData(site);
+    // make temp directory for epub
+    const tempDirPath = path.join('/tmp/', 'site-to-epub-');
+    await fs.mkdtempSync(tempDirPath);
+    
     const options = {
       title: site.title,
-      author: 'btopro',
-      publisher: 'btopro',
-      cover: "https://oer.hax.psu.edu/bto108/sites/edtechjoker/assets/banner.jpg",
-      output: `/home/bto108a/haxtheweb/${site.title}.epub`,
-      content: items
+      author: site.metadata.author.name || 'HAX The Web',
+      publisher: "HAX The Web",
+      description: site.metadata.description || '',
+      cover: base + site.metadata.site.logo || '', // use logo as cover
+      tocTitle: "Table of Contents",
+      date: site.metadata.site.updated ? new Date(site.metadata.site.updated * 1000).toISOString() : new Date().toISOString(),
+      lang: site.metadata.site.lang || 'en',
+      fetchTimeout: 3000,
+      ignoreFailedDownloads: true,
+      tempDir: tempDirPath,
     };
-    new Epub(options).promise.then(() => console.log('Done'));    
-  }
-  if (!body.cacheBuster) {
-    res = stdResponse(res, content, { cache: 86400 });
-  }
-  else {
-    res = stdResponse(res, content);  
+    content = await epub(options, items).then(
+        (contents) => {
+          return contents;
+        },
+        err => console.error(err)
+    );
+    let headers = {
+      disposition: `attachment; filename="${site.title}.epub"`,
+      type: "application/epub+zip",
+      length: content.length
+    };
+    if (!body.cacheBuster) {
+      res = stdResponse(res, content, { cache: 86400, ...headers });
+    }
+    else {
+      res = stdResponse(res, content, headers);  
+    }
   }
 }
 
@@ -51,7 +73,7 @@ export async function pagesAsData(site) {
     let content = await site.getContentById(item.id, true);
     data.push({
       title: item.title,
-      data: content
+      content: content
     });
   }
   return data;
